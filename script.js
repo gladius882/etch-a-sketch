4a26713c453ebf2c599152ca32ba61ec7e2afb0a
gladius882 <22199518+gladius882@users.noreply.github.com>
2025-05-11 14:48:19 +0200

    square div have same height and width

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ let container = document.querySelector("#sketch");
 function draw(elements) {
     container.innerHTML = '';
 
-    let size = 0;
+    let size = (970 / elements) - 2;
 
     for(let i=0; i<elements * elements; i++) {
 
         let div = document.createElement('div');
+        div.style.height = size + 'px';
+        container.appendChild(div);
 
         if( (i+1) % elements == 0 && i != 0) {
             let wrapDiv = document.createElement('div');
             wrapDiv.classList.add('wrap');
             container.appendChild(wrapDiv);
         }
-
-        container.appendChild(div);
     }
 }
 

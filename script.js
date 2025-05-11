2447e05b3801c89421a4646f70c25aef4903f823
gladius882 <22199518+gladius882@users.noreply.github.com>
2025-05-11 14:56:11 +0200

    change pixel color on mouse over

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ let container = document.querySelector("#sketch");
 function draw(elements) {
     container.innerHTML = '';
 
-    let size = (970 / elements) - 2;
+    let size = (container.offsetWidth / elements) - 2;
 
     for(let i=0; i<elements * elements; i++) {
 
         let div = document.createElement('div');
         div.style.height = size + 'px';
+
+        div.addEventListener('mouseover', onMouseOver);
+
         container.appendChild(div);
 
         if( (i+1) % elements == 0 && i != 0) {
@@ -17,6 +20,10 @@ function draw(elements) {
             container.appendChild(wrapDiv);
         }
     }
+}
+
+function onMouseOver(e) {
+    e.target.style.backgroundColor = 'red';
 }
 
 document.addEventListener('DOMContentLoaded', () => {
